7c6bfca324579b929ef289b689129bbd280493e6
Paradis <Lucasezequiel120202@gmail.com>
2022-07-28 01:59:22 -0300

    basic update

---
+++ public/js/home.js
@@ -0,0 +1,33 @@
+//var myInterval = setInterval(function () {
+//    console.log('it works' + new Date());
+//},1000); 
+
+$(() => {
+    /*
+    setTimeout(() => {
+        $("#loadMainText").text("> Loading.")
+    }, 200);
+    setTimeout(() => {
+        $("#loadMainText").text("> Loading..")
+    }, 400);
+    setTimeout(() => {
+        $("#loadMainText").text("> Loading...")
+    }, 600);
+    setTimeout(() => {
+        $("#loadMainText").text("> Loading....")
+    }, 800);
+    setTimeout(() => {
+        $("#loadMainText").text("> Loading.....")
+    }, 1000);
+
+    setTimeout(() => {
+        $("#loadMainText").toggle()
+        $("#mainHome").toggle() 
+        $("header").toggle()
+    }, 1150);
+
+    */
+    $("#loadMainText").toggle()
+    $("#mainHome").toggle() 
+    $("header").toggle()
+})
